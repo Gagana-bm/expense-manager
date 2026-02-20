911c6746b7a49ffa4d36cc5a974252338f596ef9
Gagana BM <gaganabm22@gmail.com>
2026-02-20 15:45:32 +0530

    Fix CORS properly

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors({origin:"*",credentials:true,}));
+app.use(cors({origin:"https://expense-manager-1-2lcy.onrender.com",credentials:true,}));
 
 /* ==============================
    MongoDB Connection

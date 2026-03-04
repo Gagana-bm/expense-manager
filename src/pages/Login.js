dc3a084c3c740085e36da745464af3d85c11b2f0
Gagana BM <gaganabm22@gmail.com>
2026-03-04 19:14:51 +0530

    fix hardcoded localhost in login and register

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -12,10 +12,10 @@ function Login() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", {
-        email,
-        password
-      });
+     const res = await axios.post(
+  `${process.env.REACT_APP_API_URL}/api/login`,
+  { email, password }
+);
 
       localStorage.setItem("token", res.data.token);
 
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -13,11 +13,10 @@ function Register() {
     e.preventDefault();
 
     try {
-      await axios.post("http://localhost:5000/api/register", {
-        name,
-        email,
-        password
-      });
+      await axios.post(
+  `${process.env.REACT_APP_API_URL}/api/register`,
+  { name, email, password }
+);
 
       alert("Registered Successfully 🎉");
       navigate("/");
